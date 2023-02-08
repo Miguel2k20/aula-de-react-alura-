30b52bd61f0b24e0fba92ab57820d82adddd1a1d
Miguel <joaomiguel2k20@gmail.com>
2023-02-08 20:08:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './componetes/banner';
+import Formulario from './componetes/formulario';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner></Banner>
+      <Formulario/>
     </div>
   );
 }
---
+++ src/componetes/banner/index.js
@@ -0,0 +1,9 @@
+import "./banner.css"
+function Banner(){
+    return (
+        <div className="banner">
+            <img src="../imagens/banner.png" alt="banner-principal"/>
+        </div>
+    )
+}
+export default Banner
